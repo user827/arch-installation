cee6d3ba10c8648d0ff290850d5b427a8aa03042
user827 <user827@users.noreply.github.com>
2023-12-06 21:56:56 +0200

    Switch to gpg signatures
    Because shim enforces another key to be accepted.

--- scripts/chroot/55grub.sh
+++ scripts/chroot/55grub.sh
@@ -6,12 +6,15 @@ curdir=$(cd "$(dirname "$0")" && pwd)
 . "$curdir"/../current
 
 if [ -z "${NO_SETUP_HARDWARE:-}" ]; then
-  echo 'HOOKS=(base systemd autodetect modconf kms keyboard block sd-encrypt filesystems fsck)' > /etc/mkinitcpio.conf.d/encrypted.conf
-  echo "FILES+=($KEYFILE)" >> /etc/mkinitcpio.conf.d/encrypted.conf
-  mkinitcpio -P
+echo 'HOOKS=(base systemd autodetect modconf kms keyboard block sd-encrypt filesystems fsck)' > /etc/mkinitcpio.conf.d/encrypted.conf
+echo "FILES+=($KEYFILE)" >> /etc/mkinitcpio.conf.d/encrypted.conf
+mkinitcpio -P
 fi
 
 [ -z "${NO_SETUP_HARDWARE:-}" ] || exit 0
+
+pacman -S --noconfirm sbctl edk2-shell mokutil
+DIFFPROG='nvim -d' aurmatic -S --noconfirm shim-signed
 
 (
 . /etc/default/grub
@@ -21,27 +24,23 @@ sed -ri "s|(GRUB_CMDLINE_LINUX_DEFAULT)=.*|\\1=\"$GRUB_CMDLINE_LINUX_DEFAULT rd.
 sed -ri 's/.*(GRUB_ENABLE_CRYPTODISK)=.*/\1=y/' /etc/default/grub
 mkdir -m0700 /efi
 mount "${DISK}1" /efi
-"$curdir"/../tools/updategrub.sh arch "$UUID"
-mv newgrub.cfg /boot/arch/grub/grub.cfg
-
-pacman -S --noconfirm sbctl edk2-shell mokutil
-DIFFPROG='nvim -d' aurmatic -S --noconfirm shim-signed
 
 sbctl create-keys
 openssl x509 -outform DER -in /usr/share/secureboot/keys/PK/PK.pem -out /efi/EFI/MOK.cer
+grubsign --export > "$curdir"/../grubpubkey
+
 cp /usr/share/shim-signed/shimx64.efi /efi/EFI/arch/
 cp /usr/share/shim-signed/mmx64.efi /efi/EFI/arch/
 cp /usr/share/edk2-shell/x64/Shell.efi /efi/EFI/Shellx64.efi
-sbctl sign -s /efi/EFI/arch/grubx64.efi
-sbctl sign -s /efi/EFI/Shellx64.efi
-sbctl sign -s /boot/vmlinuz-linux-my
-
-# When using own PK
-#sbctl sign -s /efi/EFI/arch/shimx64.efi
-#sbctl sign -s /efi/EFI/arch/mmx64.efi
-#sbctl enroll-keys --yes-this-might-brick-my-machine
 
 efibootmgr --unicode --disk /dev/sda --part 1 --create --label "Arch Shim" --loader /EFI/arch/shimx64.efi
+"$curdir"/../tools/updategrub.sh arch "$UUID" "$curdir"/../grubpubkey
+sbctl sign-all
+
+echo "Enroll shim MOK from ESP\\EFI\\MOK.cer"
+echo When using your own PK
+echo sbctl enroll-keys --yes-this-might-brick-my-machine
+echo And ditch shim
 
 # TODO Cannot delete the fedora key...
 #(
@@ -55,8 +54,6 @@ efibootmgr --unicode --disk /dev/sda --part 1 --create --label "Arch Shim" --loa
 #rm -r keys
 #)
 #echo "Confirm deletion of fedora key with password hello"
-
-echo "Enroll your own key from ESP\\EFI\\MOK.cer"
 
 sbctl status
 sbctl verify
--- scripts/tools/updategrub.sh
+++ scripts/tools/updategrub.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-set -eux
+set -eu
 
 name=$1
 root_uuid=$2
+pubkeypath=$3
 
 # simple
 # shim lock not needed when the kernel files are encrypted?
-grub-install --boot-directory=/boot/"$name" --target=x86_64-efi --efi-directory=/efi --bootloader-id="$name"
+grub-install --boot-directory=/boot/"$name" --target=x86_64-efi --efi-directory=/efi --bootloader-id="$name" --pubkey="$pubkeypath" --disable-shim-lock
 cfg=$(mktemp)
 uuid=$(echo "$root_uuid" | tr -d -)
+
 cat > "$cfg" <<EOF
+set check_signatures=enforce
+export check_signatures
 echo "Prefix: '\$prefix'"
 echo "Signature checking: '\$check_signatures'"
 insmod part_gpt
@@ -17,10 +21,13 @@ insmod luks2
 cryptomount -u $uuid
 configfile (crypto0)/root/boot/$name/grub/grub.cfg
 EOF
-grub-mkstandalone --format x86_64-efi -o "/efi/EFI/$name/grubx64.efi" --sbat /usr/share/grub/sbat.csv "boot/grub/grub.cfg=$cfg"
-grub-mkconfig -o newgrub.cfg
-echo "copy newgrub.cfg to /boot/$name/grub/grub.cfg"
+grub-mkstandalone --modules="gcry_sha256 gcry_sha512 gcry_dsa gcry_rsa" --format x86_64-efi -o "/efi/EFI/$name/grubx64.efi" --pubkey "$pubkeypath" --disable-shim-lock "boot/grub/grub.cfg=$cfg"
 rm "$cfg"
 
-echo TODO looses shim?
-efibootmgr -v
+sbctl sign -s /efi/EFI/"$name"/grubx64.efi
+grub-mkconfig -o "/boot/$name/grub/grub.cfg.new"
+sudo mv -b "/boot/$name/grub/grub.cfg.new" "/boot/$name/grub/grub.cfg"
+grubsign "/boot/$name/grub/grub.cfg"
+grubsign --sign-all
+
+efibootmgr
