e8b67b0fae474d99be202267d89bc02ba0a4ff62
user827 <user827@users.noreply.github.com>
2023-12-28 11:30:03 +0200

    Clean up

--- scripts/tools/encrypt_new.sh
+++ scripts/tools/encrypt_new.sh
@@ -11,8 +11,7 @@ fdisk -l "${vol%[0-9]}"
 echo "overwriting data on $vol"
 ask_continue
 
-set -- --sector-size "$SECTOR_SIZE" --use-random --type luks2 --hash sha256 --pbkdf pbkdf2 --iter-time 100
-# Grub does not support argo yet
+set -- --sector-size "$SECTOR_SIZE" --use-random --type luks2
 if [ "$BATCH" = 1 ]; then
   printf '%s' "$CRYPT_PASSWORD" | cryptsetup luksFormat --batch-mode "$@" --key-file=- "$vol"
 else
