5cd420f223cd45bb741903ecbb9d248540a70671
user827 <user827@users.noreply.github.com>
2023-12-30 12:07:36 +0200

    clean up

--- scripts/chroot/55bootloader.sh
+++ scripts/chroot/55bootloader.sh
@@ -23,28 +23,7 @@ echo 'HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block 
 
 pacman -S --noconfirm edk2-shell mokutil efibootmgr linux linux-firmware amd-ucode intel-ucode
 
-kernel_hardenings="lockdown=confidentiality edac_core.edac_mc_panic_on_ue=1 audit=1 audit_backlog_limit=8192 apparmor=1 intel_iommu=on vsyscall=none slab_nomerge mce=0 pti=on kvm-intel.vmentry_l1d_flush=always spectre_v2_user=on spec_store_bypass_disable=on lsm=landlock,lockdown,yama,safesetid,apparmor,bpf page_alloc.shuffle=1 init_on_alloc=1 init_on_free=1 iommu.passthrough=0 iommu.strict=1 randomize_kstack_offset=on mds=full random.trust_cpu=0 tsx=off efi=disable_early_pci_dma hardened_usercopy=1 vdso32=0"
-root_options="cryptdevice=UUID=$UUID:root:allow-discards"
-ls /boot/loader
-cat /boot/loader/loader.conf
-cat <<EOF > /boot/loader/loader.conf
-default  arch.conf
-timeout  4
-console-mode max
-editor   yes
-EOF
-cat <<EOF > /boot/loader/entries/arch.conf
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /initramfs-linux.img
-options root=/dev/mapper/root rw rootflags=subvol=root $root_options $kernel_hardenings
-EOF
-cat <<EOF > /boot/loader/entries/arch-fallback.conf
-title   Arch Linux (Fallback)
-linux   /vmlinuz-linux
-initrd  /initramfs-linux-fallback.img
-options root=/dev/mapper/root rw rootflags=subvol=root $root_options $kernel_hardenings
-EOF
+UUID=$UUID "$curdir"/../tools/bootloader-mkconfig.sh
 # Unified kernel image includes all those
 
 # TODO update systemd-boot hook
