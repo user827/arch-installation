1fcb85039403eacb6b0348a0ad51c84f2c4ebcdf
user827 <user827@users.noreply.github.com>
2024-01-21 18:27:45 +0200

    Update hardenings

--- scripts/tools/bootloader-mkconfig.sh
+++ scripts/tools/bootloader-mkconfig.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -eu
 : "${FOLDER:=/boot}"
-#kernel_hardenings_intel="oops=panic lockdown=confidentiality edac_core.edac_mc_panic_on_ue=1 audit=1 audit_backlog_limit=8192 apparmor=1 intel_iommu=on vsyscall=none slab_nomerge mce=0 pti=on kvm-intel.vmentry_l1d_flush=always lsm=landlock,lockdown,yama,safesetid,apparmor,bpf page_alloc.shuffle=1 init_on_alloc=1 init_on_free=1 iommu.passthrough=0 iommu.strict=1 randomize_kstack_offset=on mds=full random.trust_cpu=0 efi=disable_early_pci_dma hardened_usercopy=1 vdso32=0"
-kernel_hardenings="oops=panic lockdown=confidentiality edac_core.edac_mc_panic_on_ue=1 audit=1 audit_backlog_limit=8192 apparmor=1 amd_iommu=on intel_iommu=on vsyscall=none slab_nomerge mce=0 pti=on kvm-intel.vmentry_l1d_flush=always lsm=landlock,lockdown,yama,safesetid,apparmor,bpf page_alloc.shuffle=1 init_on_alloc=1 init_on_free=1 iommu.passthrough=0 iommu.strict=1 randomize_kstack_offset=on random.trust_cpu=0 efi=disable_early_pci_dma hardened_usercopy=1 vdso32=0"
+kernel_hardenings="oops=panic lockdown=confidentiality edac_core.edac_mc_panic_on_ue=1 audit=1 audit_backlog_limit=8192 apparmor=1 amd_iommu=on intel_iommu=on vsyscall=none slab_nomerge mce=0 pti=on kvm-intel.vmentry_l1d_flush=always lsm=landlock,lockdown,yama,safesetid,apparmor,bpf page_alloc.shuffle=1 init_on_alloc=1 init_on_free=1 iommu.passthrough=0 iommu.strict=1 randomize_kstack_offset=on random.trust_cpu=0 efi=disable_early_pci_dma hardened_usercopy=1 vdso32=0 mitigations=auto"
 root_options="root=/dev/mapper/root cryptdevice=UUID=$UUID:root:allow-discards rw rootflags=subvol=root ${BOOTLOADER_OPTS:-}"
 cat <<EOF > "$FOLDER/loader/loader.conf"
 default  arch.conf
